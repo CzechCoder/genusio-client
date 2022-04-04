4607e6b1d01019c9904fa5f3859aebfd7cd260d3
CzechCoder <92126675+CzechCoder@users.noreply.github.com>
2022-04-04 16:02:25 +0200

    Add files via upload

---
+++ src/api/community.js
@@ -0,0 +1,54 @@
+// register community
+export const registerComm = async ({
+  communityname,
+        uniqueValue,
+        image,
+        foundingDate,
+        membersCount,
+        commCategoriess,
+        growthToolss,
+        otherTools,
+        membershipPayment,
+        regIntentionn 
+} = {}) => {
+    const user = {
+      communityname,
+      uniqueValue,
+      image,
+      foundingDate,
+      membersCount,
+      commCategoriess,
+      growthToolss,
+      otherTools,
+      membershipPayment,
+      regIntentionn
+  };
+  
+    try {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/communities`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+  
+      return await res.json();
+    } catch (err) {
+      throw new Error("Cannot register at this time.");
+    }
+  };
+  
+   // get community details
+   export const getComm = async () => {
+       try {
+           const res = await fetch(`${process.env.REACT_APP_API_URL}/community`, {
+               method: "GET",
+               credentials: "include",
+           });
+           return await res.json();
+       } catch(err) {
+          throw new Error("Please login to continue")
+       }
+   }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,36 @@
+// this is the window that opens when the katalog item is clicked
+
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+
+const Commodal = ({ selectedKom, setSelectedImg }) => {
+  const handleHideClick = (e) => {
+    setSelectedImg(null);
+  };
+
+  return (
+    <>
+      <div className="basemodal">
+        <div className="modalwrapper">
+          <div className="main-content">
+            <div className="mod-topbar">
+              <CloseIcon onClick={handleHideClick} className="close-it" />
+            </div>
+            <div className="comm-grid">
+              <img src={selectedKom.image} alt="think bigger!" />
+              <div className="comm-desc">
+                <h4>
+                  <b>{selectedKom.communityname}</b>
+                </h4>
+                <p>{selectedKom.uniqueValue}</p>
+              </div>
+            </div>
+          </div>
+          <div className="closer" onClick={handleHideClick}></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Commodal;
